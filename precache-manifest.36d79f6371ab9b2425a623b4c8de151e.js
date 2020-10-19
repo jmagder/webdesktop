5c758eee92a04b47cd6d4d39a7bdc7b86909213d
jeff <jeff.magder@gmail.com>
2020-10-19 13:36:42 -0500

    Prod build of web-desktop

---
+++ precache-manifest.36d79f6371ab9b2425a623b4c8de151e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd9dd0e6715a0b40d938da98ac9501a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a1f142f0d35d28f4b87b",
+    "url": "./static/css/main.4a76347b.chunk.css"
+  },
+  {
+    "revision": "48aa32fef8a57de71d41",
+    "url": "./static/js/2.270a3172.chunk.js"
+  },
+  {
+    "revision": "c83eaf460a8f55ef068d9679da1b7145",
+    "url": "./static/js/2.270a3172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f142f0d35d28f4b87b",
+    "url": "./static/js/main.e41e74a3.chunk.js"
+  },
+  {
+    "revision": "d5fd38327899e542c1f5",
+    "url": "./static/js/runtime-main.fbaeaec2.js"
+  },
+  {
+    "revision": "dd2a9f4783f3b171e95fe896b299c370",
+    "url": "./static/media/bar-chart.dd2a9f47.svg"
+  },
+  {
+    "revision": "87c9d8519f96720e429d6815de725a61",
+    "url": "./static/media/note.87c9d851.svg"
+  },
+  {
+    "revision": "19c1944f453b191ad74123a25721f55a",
+    "url": "./static/media/pie-chart.19c1944f.svg"
+  },
+  {
+    "revision": "4f698a148870032f8c94dfba3d87fa21",
+    "url": "./static/media/settings.4f698a14.svg"
+  },
+  {
+    "revision": "825e1d134281afd9e2b603bb3e3b2d73",
+    "url": "./static/media/start.825e1d13.svg"
+  }
+]);
